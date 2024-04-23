41121fb8df9cb2534fbd7ec05085f1f0e5e1801d
Jaku6M <j.mazurkiewicz13@wp.pl>
2024-04-23 19:57:11 +0200

    Output from the topic /Joint_States is saved as a txt files hopefully it will be willing to save itself to csv

--- launch/MX28ARoptimization.py
+++ launch/MX28ARoptimization.py
@@ -8,7 +8,7 @@ def launch_gazebo_simulation():
     # Open a new terminal and launch Gazebo simulation
     subprocess.Popen(['gnome-terminal', '--', 'ros2', 'launch', 'one_dynamixel_simulation', 'MX28AR_gazebo.launch.py'])
 
-def send_joint_trajectory_command(feedback_file_path):
+def send_joint_trajectory_command():
     # Wait for 5 seconds before sending the joint trajectory command
     time.sleep(4)
 
@@ -29,19 +29,17 @@ def send_joint_trajectory_command(feedback_file_path):
     # Convert the trajectory dictionary to a JSON-formatted string
     trajectory_json = json.dumps(trajectory)
 
-    # subprocess.Popen(['gnome-terminal', '--','ros2', 'action', 'send_goal', '/joint_trajectory_controller/follow_joint_trajectory', 'control_msgs/action/FollowJointTrajectory', '-f', trajectory_json])
+    subprocess.Popen(['gnome-terminal', '--','ros2', 'action', 'send_goal', '/joint_trajectory_controller/follow_joint_trajectory', 'control_msgs/action/FollowJointTrajectory', '-f', trajectory_json])
+
+def save_trajectory_data(feedback_file_path):
     # Open a new process to execute the ROS command and capture output
     with open(feedback_file_path, 'w') as output_file:
         process = subprocess.Popen(
-            ['ros2', 'action', 'send_goal', '/joint_trajectory_controller/follow_joint_trajectory', 
-             'control_msgs/action/FollowJointTrajectory', '-f', trajectory_json], 
+            ['ros2', 'topic', 'echo', '/joint_states'], 
             stdout=output_file,
             stderr=subprocess.PIPE,
             universal_newlines=True
         )
-        # Wait for the process to complete
-        process.wait()
-
 
 if __name__ == '__main__':
     launch_gazebo_simulation()
@@ -49,5 +47,6 @@ if __name__ == '__main__':
     feedback_file_path = '/home/jaku6m/Desktop/OPTYMALIZACJA/OptcsvGazeboFiles/output_file.txt'  # Path to the feedback text file
     output_csv_file = '/home/jaku6m/Desktop/OPTYMALIZACJA/OptcsvGazeboFiles/feedback_data.csv'  # Path to the output CSV file
     # Send the joint trajectory command and capture feedback
-    send_joint_trajectory_command(feedback_file_path) 
+    send_joint_trajectory_command() 
+    save_trajectory_data(feedback_file_path)
 
